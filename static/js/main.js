d3014a667e90aca7a3e11df67012453bf4132d6d
AbdEl-Rahman <3bdo.kha@gmail.com>
2022-03-10 23:42:48 +0200

    custom dialogs

--- static/js/main.js
+++ static/js/main.js
@@ -1,28 +1,40 @@
-import { GameEngine } from "./GameEngine.js";
-import { AIGameEngine } from "./AIGameEngine.js";
-import { Point } from "./Point.js";
-import { render } from "./view.js"
+import {GameEngine} from "./GameEngine.js";
+import {AIGameEngine} from "./AIGameEngine.js";
+import {Point} from "./Point.js";
+import {render} from "./view.js"
 
 // red 1, yellow 1
 let _board = [];
-let engine, board, search, p1, p2;
+let engine, board, search, p1, p2, dialog_prev_data, btn_show_prev, btn_close, dialog_result, res_msg, time, prev_p1,
+    prev_p2, prev_res;
 let started = false;
 let isAI;
 let isAiTurn = false;
 
 
 window.addEventListener("load", function () {
-    // get p1 and p2
+    // select some html elements
+    board = document.getElementById("game-board");
     p1 = document.getElementById('p1');
     p2 = document.getElementById('p2');
+    dialog_prev_data = document.getElementById('dialog-prev');
+    dialog_result = document.getElementById('dialog-result');
+    btn_show_prev = document.getElementById('show-prev');
+    btn_close = document.getElementById('btnClose');
+    res_msg = document.getElementById('game-result-msg');
+    time = document.getElementById('time');
+    prev_p1 = document.getElementById('op1');
+    prev_p2 = document.getElementById('op2');
+    prev_res = document.getElementById('ores');
+
     // get values from search
     search = location.search.split("&");
+
     // render board
     render();
     // initialize the game logic
     initLogic();
-    // get board
-    board = document.getElementById("game-board");
+
     // add on click to column
     document.querySelectorAll('.column').forEach(col => {
         col.addEventListener("click", () => {
@@ -35,7 +47,23 @@ window.addEventListener("load", function () {
     btn.addEventListener('click', function (e) {
         started = true;
         btn.hidden = true
+        saveNewData();
     }); // end event
+
+    // button show prev game data
+    btn_show_prev.addEventListener('click', ev => {
+        showDialog(dialog_prev_data);
+    });
+
+    // close btn on dialog
+    btn_close.addEventListener('click', ev => {
+        dialog_prev_data.classList.add('hide-dialog');
+        dialog_prev_data.classList.remove('dialog-cont');
+    });
+
+    document.getElementById('btnRePlay').addEventListener('click', ev => {
+        location.reload();
+    });
 
 }); // end load
 
@@ -55,9 +83,11 @@ function initLogic() {
     engine.level = Number(getValue('gameLevel'));
     // set names
     p1.innerHTML = getValue('p1');
-    if (!isAI){
+    if (!isAI) {
         p2.innerHTML = getValue('p2');
     }
+    // load data from local storage
+    loadSaved();
 }
 
 function setColor(col_no) {
@@ -114,10 +144,9 @@ function changeTurn() {
     _board[engine.move.row][engine.move.col] = engine.colors[engine.current_color];
     //update board in game engine
     engine.board = _board;
-
     //console.log(engine.checkDiagonal(engine.colors[engine.current_color]));
     if (engine.checkWinner(engine.colors[engine.current_color])) {
-        alert(`${engine.colors[engine.current_color]} wins`);
+        showGameResult(engine.current_color);
     }
     // change color
     if (engine.current_color === 'red') {
@@ -126,6 +155,37 @@ function changeTurn() {
         engine.current_color = 'red'
     }
     isAiTurn = !isAiTurn;
+}
+
+function showGameResult(result) {
+    if (result !== 'draw') {
+        let winner = getValue('p' + engine.colors[result]);
+        res_msg.innerHTML = `<span class="color_${result}">${winner}</span> Won !`
+        window.localStorage.setItem('status', winner + ' Won');
+    } else {
+        res_msg.innerHTML = `Draw !`
+    }
+    showDialog(dialog_result)
+
+}
+
+function saveNewData() {
+    window.localStorage.setItem('date', new Date().toLocaleDateString())
+    window.localStorage.setItem('p1', getValue('p1'));
+    window.localStorage.setItem('p2', getValue('p2'));
+    window.localStorage.setItem('status', 'Not Completed');
+}
+
+function loadSaved() {
+    prev_p1.innerHTML = window.localStorage.getItem('p1');
+    prev_p2.innerHTML = window.localStorage.getItem('p2');
+    time.innerHTML = window.localStorage.getItem('date');
+    prev_res.innerHTML = window.localStorage.getItem('status');
+}
+
+function showDialog(d) {
+    d.classList.remove('hide-dialog');
+    d.classList.add('dialog-cont');
 }
 
 // to get value from search by key
